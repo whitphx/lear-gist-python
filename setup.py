2ddbbeadcdc71934a3e3e0cb253d92d2fbb2be0a
tuttieee <t.yic.yt@gmail.com>
2018-08-28 02:08:34 +0900

    Update setup.py to satisfy flake8

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import numpy
 from distutils.core import setup
 from distutils.extension import Extension
-from distutils.command import build_ext
 
 gistmodule = Extension(
-	'gist',
-	sources=['lear_gist-1.2/gist.c', 'lear_gist-1.2/standalone_image.c', 'gistmodule.c'],
-	extra_compile_args=['-DUSE_GIST', '-DSTANDALONE_GIST'],
-	include_dirs=[numpy.get_include()],
-	libraries=['fftw3f'])
+    'gist',
+    sources=['lear_gist-1.2/gist.c', 'lear_gist-1.2/standalone_image.c',
+             'gistmodule.c'],
+    extra_compile_args=['-DUSE_GIST', '-DSTANDALONE_GIST'],
+    include_dirs=[numpy.get_include()],
+    libraries=['fftw3f'])
 
 setup(name='gist',
-	version='0.1',
-	description='A wrapper package of lear_gist',
-	ext_modules=[gistmodule])
+      version='0.1',
+      description='A wrapper package of lear_gist',
+      ext_modules=[gistmodule])
