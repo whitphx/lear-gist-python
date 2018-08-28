a3681ab1ca66e2eddcde11deef3b5fe1d77159ea
tuttieee <t.yic.yt@gmail.com>
2018-08-29 00:07:04 +0900

    Add ValueTestCase

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import unittest
+import os.path
 
 import numpy as np
 
 import gist
 
 
-class GistTestCase(unittest.TestCase):
+DATADIR = os.path.join(os.path.dirname(__file__), 'test/data')
+
+
+class FunctionalityTestCase(unittest.TestCase):
     def test_with_zero_array(self):
         black_image = np.zeros((480, 640, 3), dtype=np.uint8)
         gist.extract(black_image)
@@ -13,6 +17,23 @@ class GistTestCase(unittest.TestCase):
     def test_with_None(self):
         with self.assertRaises(TypeError):
             self.assertIsNone(gist.extract(None))
+
+
+class ValueTestCase(unittest.TestCase):
+    def load_reference(self, path):
+        with open(path) as f:
+            content = f.read()
+
+        arr = np.array([float(elem) for elem in content.split()])
+        return arr
+
+    def test(self):
+        arr = np.load(os.path.join(DATADIR, 'scene.npy'))
+        result = gist.extract(arr)
+
+        reference = self.load_reference(
+            os.path.join(DATADIR, 'scene.no_arg.result'))
+        np.testing.assert_allclose(reference, result, rtol=1e-04, atol=1e-04)
 
 
 if __name__ == '__main__':
